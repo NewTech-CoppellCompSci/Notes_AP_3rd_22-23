4a55a4a4d28489f067a55caa6a74a841f175db38
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2023-01-25 12:33:55 -0600

    Create Notes_Search.java

---
+++ src/notes09_Search_Sort/Notes_Search.java
@@ -0,0 +1,37 @@
+package notes09_Search_Sort;
+
+public class Notes_Search {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Binary Search Notes
+		 */
+		
+		
+		
+		//get your necessary variables
+		//max = top, min = 0, mid, key
+		
+		//loop until mid == key or min > max
+			
+			//calculate mid (average min and max
+		
+			//check mid value in list
+		
+			//value at mid > key
+				//max = mid - 1
+		
+			//value at mid < key
+				//min = mid + 1
+				
+			//value at mid == key
+				//found it
+				//stop loop or show you've found in the index, return that index
+		
+		
+		
+		
+	}
+	
+}
